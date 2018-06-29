e8a3b7152ca195a61278ee955aeb5e4b07c88883
girishhp2417 <40119705+girishhp2417@users.noreply.github.com>
2018-06-29 10:41:54 +0530

    adding new node at the beginning, at the end and after any node and deleting nodes at a given position and when a key is given

--- creating and printing the list.py
+++ creating and printing the list.py
@@ -28,7 +28,45 @@ class LinkedList:
 
         new_node.next=None
         last.next=new_node
+
+    def deletepos(self,position):
+        temp=self.head
+        if(position==0):
+            self.head=temp.next
+            temp=None
+            return
+
+        for i in range(position-1):
+            temp=temp.next
+            if(temp is None):
+                break
+        if(temp is None):
+            return
+        if(temp.next is None):
+            return
+
+        next=temp.next.next
+        temp.next=None
+        temp.next=next
+
+    def deletekey(self,key):
+        temp=self.head
+
+        if(temp.data==key):
+            self.head=temp.next
+            temp=None
+            return
+        
+        while temp is not none:
+            if(temp.data==key):
+                break
+            prev=temp
+            temp=temp.next
+
+        prev.next=temp.next
+        temp=None
  
+
     def printList(self):
         temp = self.head
         while (temp):
@@ -45,6 +83,7 @@ if __name__=='__main__':
     llist.append(109)
     llist.addafter(llist.head.next,234)
     llist.addafter(llist.head.next.next,453)
+    llist.deletepos(2)
 
 
     llist.printList()
